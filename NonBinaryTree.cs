fc7aba230887e1fe206098a16862070d27543d38
Nikolay Nekrasov <nikolaynekrasov1999@gmail.com>
2021-10-03 20:56:37 +0300

    I've just started to code this.

---
+++ NonBinaryTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class NonBinaryTree<T>
+    {
+        NonBinaryTreeNode<T> root;
+        public NonBinaryTree(T value)
+        {
+            root = new NonBinaryTreeNode<T>(value);
+        }
+    }
+}
---
+++ NonBinaryTreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures
+{
+    class NonBinaryTreeNode<T>
+    {
+        T value;
+        List<T> branches;
+        public NonBinaryTreeNode(T value)
+        {
+            this.value = value;
+            branches = new List<T>();
+        }
+    }
+}
