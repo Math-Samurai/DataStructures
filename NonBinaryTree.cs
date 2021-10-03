2c697d065ad4b5f417b3f9072d65ea405999ca43
Nikolay Nekrasov <nikolaynekrasov1999@gmail.com>
2021-10-03 23:21:14 +0300

    1.03 version of NonBinaryTree

---
+++ NonBinaryTree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures
+{
+    class NonBinaryTree<T>
+    {
+        T value;
+        List<NonBinaryTree<T>> branches;
+        public NonBinaryTree(T rootValue)
+        {
+            value = rootValue;
+            branches = new List<NonBinaryTree<T>>();
+        }
+        public void Add(T newValue)
+        {
+            NonBinaryTree<T> newBranch = new NonBinaryTree<T>(newValue);
+            branches.Add(newBranch);
+        }
+        public List<NonBinaryTree<T>> Branches
+        {
+            get
+            {
+                return branches;
+            }
+        }
+    }
+}
